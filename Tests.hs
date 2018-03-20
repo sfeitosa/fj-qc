bf1eff45b63f0fbeb0d4b8d99b84feaef5922be1
Samuel Feitosa <samuel.feitosa@ifsc.edu.br>
2018-03-20 14:14:36 -0300

    Adding class table generation and test for properties of cast-safe expressions and well-formed class table.

--- Tests.hs
+++ Tests.hs
@@ -7,17 +7,11 @@ import FJParser
 import FJUtils
 import FJInterpreter
 import FJTypeChecker
-import CT
 import Test.QuickCheck
 import Data.Map
 import Data.List
 import Data.Maybe
 import Control.Monad
-
--- Function: genVar
--------------------
-genVar :: Gen Char
-genVar = elements ['a' .. 'z']
 
 -- Function: maybeElements
 -- Objective: Get one element of a given list.
@@ -35,6 +29,142 @@ maybeElements xs = Just (elements xs)
 -----------------------------------------------------------
 pickVar :: Env -> Type -> Maybe (Gen Expr)
 pickVar ctx t = maybeElements [Var x | (x, t') <- Data.Map.assocs ctx, t' == t]
+
+-- Function: genClassName
+-- Objective: Generate a random class name.
+-- Params: None.
+-- Returns: A string containing a valid class name.
+--------------------------------------------------
+genClassName :: Gen String
+genClassName = do n <- elements ['A' .. 'Z']
+                  return [n]
+
+-- Function: genType
+-- Objective: Generate a type from the class table.
+-- Params: None.
+-- Returns: A string containing a class name.
+---------------------------------------------------
+genType :: CT -> Gen String
+genType ct = elements ("Object" : keys ct)
+
+-- Function: genVar
+-- Objective: Generate a random variable name.
+-- Params: None
+-- Returns: A string containing a valid variable name.
+-----------------------------------------------------
+genVar :: Gen String
+genVar = do n <- elements ['a' .. 'z']
+            return [n]
+
+-- Function: genAttrs
+-- Objective: Generate a list of type/attributes for a given class.
+-- Params: Class table, class name, base class.
+-- Returns: A list of type/attributes.
+-------------------------------------------------------------------
+genAttrs :: CT -> String -> String -> Gen [(String,String)]
+genAttrs ct c b = 
+  do n <- choose (0,5)
+     tl <- vectorOf n (genType ct)
+     vl <- (vectorOf n (genVar))
+     tl' <- Control.Monad.mapM (\n -> if (subtyping n c ct) then 
+                                        return "Object"
+                                      else 
+                                        return n
+                                ) tl
+     case (fields b ct) of 
+       Just bflds -> return (zip tl' 
+                       ((nub vl) Data.List.\\ (snd (unzip bflds))))
+       _ -> return (zip tl' (nub vl))
+
+-- Function: genMethod
+-- Objective: Generate a random method for a given class.
+-- Params: Class table, class name, base class.
+-- Returns: A generated method.
+---------------------------------------------------------
+genMethod :: CT -> String -> String -> Gen Method
+genMethod ct c b = 
+  do n <- choose (0,3) -- Number of params
+     tl <- vectorOf n (genType ct) -- Type of params
+     vl <- vectorOf n (genVar) -- Name of params
+     m <- genVar -- Method name
+     r <- genType ct -- Return type 
+     e <- genExpression ct -- Body expression
+            (fromList (("this", TypeClass c) :
+              (zip (nub vl) (Data.List.map (\t -> TypeClass t) tl)))
+            ) r
+     return (Method 
+               (TypeClass r) 
+               ('m':m)  
+               (zip (Data.List.map (\t -> TypeClass t) tl) (nub vl))
+               e
+            )
+
+-- Function: genMethods
+-- Objective: Generate a list of random methods.
+-- Params: Class table, class name, base class.
+-- Returns: A list of methods.
+------------------------------------------------
+genMethods :: CT -> String -> String -> Gen [Method]
+genMethods ct c b = 
+ do n <- choose (0,3) -- Number of methods
+    ml <- vectorOf n (genMethod ct c b) 
+    return (deleteFirstsBy (\(Method _ n' _ _) (Method _ n'' _ _) -> n' == n'') 
+             (nubBy (\(Method _ n' _ _) (Method _ n'' _ _) -> n' == n'') ml)
+              bml)
+  where bml = case (methods b ct) of 
+                Just meths -> meths
+                _ -> []
+
+-- Function: formatConstr
+-- Objective: Format a class constructor.
+-- Params: Class table, class name, base class, class attributes.
+-- Returns: A constructor AST for the given class. 
+-----------------------------------------------------------------
+formatConstr :: CT -> String -> String -> [(Type,String)] -> Constr
+formatConstr ct c b attrs = 
+  case (fields b ct) of 
+    Just flds -> Constr c -- Class name
+                        (flds ++ attrs) -- Constr parameters
+                        (snd (unzip flds)) -- Super parameters
+                        (zip (snd (unzip attrs)) (snd (unzip attrs))) -- this
+    _ -> Constr c attrs [] (zip (snd (unzip attrs)) (snd (unzip attrs)))
+
+-- Function: genClass
+-- Objective: Generate a random class with attributes, constructor and methods.
+-- Params: Class table, class name, base class.
+-- Returns: A generated class.
+-------------------------------------------------------------------------------
+genClass :: CT -> String -> String -> Gen Class
+genClass ct c b = 
+  do attrs <- genAttrs ct c b
+     meths <- genMethods ct c b
+     return (Class c b 
+              (Data.List.map (\(k,p) -> (TypeClass k,p)) attrs) -- Attributes
+              (formatConstr ct c b -- Constructor
+                (Data.List.map (\(k,p) -> (TypeClass k,p)) attrs)) -- Params
+              meths) -- Methods
+
+-- Function: addClass
+-- Objective: Generate a class with the given name and adds to the class table.
+-- Params: Class table, class name.
+-- Returns: A class table containing the generated class.
+-------------------------------------------------------------------------------
+addClass :: CT -> String -> Gen CT
+addClass ct nm = do cb <- genType ct
+                    cl <- genClass ct nm cb
+                    return (Data.Map.insert nm cl ct)
+
+-- Function: genClassTable
+-- Objective: Generate a random class table.
+-- Params: None.
+-- Returns: A random class table.
+--------------------------------------------
+genClassTable :: Gen CT
+genClassTable = do n <- choose (1,10)
+                   ns <- vectorOf n (genClassName)
+                   ct <- Control.Monad.foldM (addClass) Data.Map.empty (nub ns)
+                   return ct
+                   
 
 -- Function: genFieldAccess
 -- Objective: Generate an expression using FieldAccess constructor.
@@ -81,7 +211,7 @@ cfields size ctx ct t =
                              []        
                          ) lst
              _ -> []
-    ) (keys ct) 
+    ) ("Object" : keys ct) 
 
 -- Function: cmethods
 -- Objective: Generate a list of candidates for MethodInvk constructor.
@@ -101,7 +231,7 @@ cmethods size ctx ct t =
                    []
                ) lst
              _ -> []
-    ) (keys ct)
+    ) ("Object" : keys ct)
 
 
 -- Function: cucast
@@ -116,7 +246,7 @@ cucast size ctx ct t =
              [genCast size ctx ct t k]
            else 
              []  
-    ) (keys ct)
+    ) ("Object" : keys ct)
 
 -- Function: cdcast
 -- Objective: Generate a list of candidates for Cast constructor.
@@ -157,11 +287,11 @@ genExpr size ct ctx t
       oneof ([do el <- Control.Monad.mapM (genExpr (size `div` 2) ct ctx) ft
                  return (CreateObject t el)] ++ 
               maybeToList (pickVar ctx (TypeClass t)) ++ 
-              fc ++ mc ++ ucc ++ dcc)
+              fc ++ mc ++ ucc)
   | otherwise = do el <- Control.Monad.mapM (genExpr (size `div` 2) ct ctx) ft
                    oneof ([return (CreateObject t el)] ++ 
                        maybeToList (pickVar ctx (TypeClass t)))
-    where ft = case (fields t classtable) of
+    where ft = case (fields t ct) of
                  Just lst -> Data.List.map (\((TypeClass t'),n') -> t') lst
                  _ -> []
           -- FieldAccess candidates 
@@ -189,62 +319,84 @@ genExpr size ct ctx t
                 else
                   []
 
--- Function: genType
--- Objective: Generate a type from the class table.
--- Params: None.
--- Returns: A string containing a class name.
----------------------------------------------------
-genType :: Gen String
-genType = elements (keys classtable)
-
 -- Function: genExpression
 -- Objective: Generate an expression of a given type.
 -- Params: Class name.
 -- Returns: An expression of a given type.
 -----------------------------------------------------
-genExpression :: String -> Gen Expr
-genExpression t = sized (\n -> genExpr n classtable Data.Map.empty t)
+genExpression :: CT -> Env -> String -> Gen Expr
+genExpression ct ctx t = sized (\n -> genExpr n ct ctx t)
 
 -- Type-class: Arbitrary Expr
 -- Objective: Generate random well-typed expressions.
 -----------------------------------------------------
 instance Arbitrary Expr where
-  arbitrary = do t <- elements (keys classtable)
-                 sized (\n -> genExpr n classtable Data.Map.empty t)
+  arbitrary = do ct <- genClassTable
+                 t <- elements ("Object" : keys ct)
+                 sized (\n -> genExpr n ct Data.Map.empty t)
 
--- Function: prop_genwelltyped
+-- Function: prop_genwelltypedct
+-- Objective: Test if the generated class table is well-formed.
+-- Params: None.
+-- Returns: True if the class tables are well-formed, False otherwise.
+----------------------------------------------------------------------
+prop_genwellformedct =
+  forAll (genClassTable) $
+    \ct -> Data.List.all 
+             (\(c,cl) -> classTyping cl Data.Map.empty ct) (Data.Map.toList ct)
+
+-- Function: prop_gencastsafeexpr
+-- Objective: Test if the generated expressions are cast-safe.
+-- Params: None.
+-- Returns: True if the expression is cast-safe, False otherwise.
+-----------------------------------------------------------------
+prop_gencastsafeexpr = 
+  forAll (genClassTable) $ 
+    \ct -> forAll (genType ct) $
+    \t -> forAll (genExpression ct Data.Map.empty t) $
+    \e -> case e of 
+            (Cast c e) -> case (typeof Data.Map.empty ct e) of
+                            Right (TypeClass t') -> subtyping t' c ct
+                            _ -> False
+            _ -> True
+
+-- Function: prop_genwelltypedexpr
 -- Objective: Test if the generated expressions are well-typed.
 -- Params: None.
 -- Returns: True if the expressions are well-typed, False otherwise.
 --------------------------------------------------------------------
-prop_genwelltyped = forAll genType $ 
-                      \t -> forAll (genExpression t) $ 
-                      \e -> either (const False)
-                                   (\(TypeClass t') -> t == t')
-                                   (typeof Data.Map.empty classtable e)
+prop_genwelltypedexpr =
+  forAll (genClassTable) $
+    \ct -> forAll (genType ct) $ 
+    \t -> forAll (genExpression ct Data.Map.empty t) $ 
+    \e -> either (const False)
+                 (\(TypeClass t') -> t == t')
+                 (typeof Data.Map.empty ct e)
+
+-- Function: prop_progress
+-- Objective: Tests for the property of progress.
+-- Params: None.
+-- Returns: True if the property held for the test casts, False otherwise.
+-------------------------------------------------------------------------- 
+prop_progress =
+  forAll (genClassTable) $
+    \ct -> forAll (genType ct) $
+    \t -> forAll (genExpression ct Data.Map.empty t) $
+    \e -> isValue ct e || maybe (False) (const True) (eval' ct e)
 
 -- Function: prop_preservation
 -- Objective: Tests for the property of preservation.
 -- Params: None.
 -- Returns: True if the property held for the test cases, False otherwise.
 --------------------------------------------------------------------------
-prop_preservation = 
-  forAll genType $ 
-    \t -> forAll (genExpression t) $
+prop_preservation =
+  forAll (genClassTable) $
+    \ct -> forAll (genType ct) $ 
+    \t -> forAll (genExpression ct Data.Map.empty t) $
     \e -> either (const False)
-                 (\(TypeClass t') -> t == t')
-                 (case (eval' classtable e) of 
-                    Just e' -> typeof Data.Map.empty classtable e'
+                 (\(TypeClass t') -> subtyping t' t ct)
+                 (case (eval' ct e) of 
+                    Just e' -> typeof Data.Map.empty ct e'
                     _ -> throwError (UnknownError e)
                  )
-
--- Function: prop_progress
--- Objective: Tests for the property of progress.
--- None.
--- Returns: True if the property held for the test casts, False otherwise.
--------------------------------------------------------------------------- 
-prop_progress =
- forAll genType $
-   \t -> forAll (genExpression t) $
-   \e -> isValue classtable e || maybe (False) (const True) (eval' classtable e)
 
